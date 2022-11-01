e5e86b1ff40ea556e453f2fbb7df39e27a0e22dd
DESKTOP-E0AE5J4\NAMUIT <20521631@gm.uit.edu.vn>
2022-11-01 09:59:31 +0700

    carousel banner

---
+++ BookingHotel/BookingHotel/Models/Banner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingHotel.Models
+{
+    public class Banner
+    {
+        public string Image { get; set; }
+    }
+}
