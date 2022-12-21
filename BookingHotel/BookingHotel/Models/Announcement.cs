8d3e8574a7333fae5b35c1122125fde7b32dd874
gameitthoi <lamdat3302@gmail.com>
2022-12-21 18:01:58 +0700

    logo, shadow collectionview

---
+++ BookingHotel/BookingHotel/Models/Announcement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingHotel.Models
+{
+    public class Announcement
+    {
+        public string Hinh { get; set; }
+        public string Title { get; set; }   
+        public string Content { get; set; }
+    }
+}
--- BookingHotel/BookingHotel/Views/Page_Announcement.xaml.cs
+++ BookingHotel/BookingHotel/Views/Page_Announcement.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BookingHotel.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,21 @@ namespace BookingHotel.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Page_Announcement : ContentPage
     {
+        public ObservableCollection<Announcement> dstb;
+        void hienthitb()
+        {
+            dstb = new ObservableCollection<Announcement>();
+           
+            dstb.Add(new Announcement { Hinh = "dat_success.png", Title = "Bạn đã đặt vé thành công", Content="Vé khách sạn 1 đêm Hải Âu của bạn đã được đặt thành công!"});
+            dstb.Add(new Announcement { Hinh = "dat_warning.png", Title = "Bạn đã đặt vé thành công", Content="Vé khách sạn 1 đêm Hải Âu của bạn đã được đặt thành công!"});
+            dstb.Add(new Announcement { Hinh = "dat_fail.png", Title = "Bạn đã đặt vé thành công", Content="Vé khách sạn 1 đêm Hải Âu của bạn đã được đặt thành công!"});
+
+            Announcement_Collection.ItemsSource = dstb;
+        }
         public Page_Announcement()
         {
             InitializeComponent();
+            hienthitb();
         }
 
         private async void back_btn_Clicked(object sender, EventArgs e)
