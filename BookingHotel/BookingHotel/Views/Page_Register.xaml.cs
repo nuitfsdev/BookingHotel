fc171b6d77cd5d5386b55965f69562ca7fb1df70
gameitthoi <lamdat3302@gmail.com>
2022-12-21 22:04:42 +0700

    register va filter

---
+++ BookingHotel/BookingHotel/Views/Page_Register.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BookingHotel.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page_Register : ContentPage
+    {
+        public Page_Register()
+        {
+            InitializeComponent();
+        }
+
+        private void login_btn_Tapped(object sender, EventArgs e)
+        {
+            Shell.Current.GoToAsync(state: "//login");
+        }
+
+        private void Register_btn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
