6de61af0807e529923979bd3687990dad4f009b8
nuitfsdev <20521631@gm.uit.edu.vn>
2022-12-31 02:08:57 +0700

    edit bookingpage

--- BookingHotel/BookingHotel.Android/MainActivity.cs
+++ BookingHotel/BookingHotel.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using FFImageLoading.Forms.Platform;
 
 namespace BookingHotel.Droid
 {
@@ -16,6 +17,7 @@ namespace BookingHotel.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            CachedImageRenderer.Init(enableFastRenderer: true);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
