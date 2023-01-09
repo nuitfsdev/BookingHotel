a1a96a28b6c9d5b12d0d1e1c4fd412e3a7f96c63
nuitfsdev <20521631@gm.uit.edu.vn>
2023-01-09 23:50:03 +0700

    edit booking info

--- BookingHotel/BookingHotel/Views/Page_Order_Detail.xaml.cs
+++ BookingHotel/BookingHotel/Views/Page_Order_Detail.xaml.cs
@@ -18,45 +18,66 @@ namespace BookingHotel.Views
         Hotel thishotel;
         Room thisroom;
 
-        async void hienthi(Order order)
+        void hienthi(HoaDon order)
         {
-            HttpClient httpClient = new HttpClient();
-            var Hotel = await httpClient.GetStringAsync($"https://bookinghotel.onrender.com/hotels?maht={order.maht}");
-            var HotelConverted = JsonConvert.DeserializeObject<List<Hotel>>(Hotel);
-            thishotel = HotelConverted[0];
+            thishotel = order.hotel;
+            thisroom = order.room;
 
             //var Room = await httpClient.GetStringAsync($"https://bookinghotel.onrender.com/room?maroom={order.maroom}");
             //var RoomConverted = JsonConvert.DeserializeObject<List<Room>>(Room);
             //thisroom = RoomConverted;
 
-            hinh.Source = thishotel.hinh[0].ToString();
-            tenKS.Text = thishotel.tenht;
-            tenphong.Text = "Phòng Cơ bản";
+            hinh.Source = order.hotel.hinh[0].ToString();
+            tenKS.Text = order.hotel.tenht;
+            tenphong.Text = order.room.tenphong;
             diachi.Text = thishotel.diachi;
             rate.Text = thishotel.sosao;
             
-            adult_qty.Text = String.Format("{0}", order.adult);
-            child_qty.Text = String.Format("{0}", order.child);
-            bed_qty.Text = String.Format("{0}", order.bed);
-            checkin_date.Text = order.checkin;
-            checkout_date.Text = order.checkout;
-            timecheckin_date.Text = order.timecheckin;
-            timecheckout_date.Text= order.timecheckout;
-            create_date.Text = order.create;
-            if (order.status == 1) cancle_booking.IsVisible = true;
+            adult_qty.Text = String.Format("{0}", order.slnguoilon);
+            child_qty.Text = String.Format("{0}", order.sltreem);
+            bed_qty.Text = String.Format("{0}", order.slphong);
+            checkin_date.Text = order.ngaynhan;
+            checkout_date.Text = order.ngaytra;
+            timecheckin_date.Text = order.gionhan;
+            timecheckout_date.Text= order.giotra;
+            create_date.Text = order.ngayhd;
+            ptthanhtoan.Text = order.phuongthuc;
+            tenkh.Text = order.tenkh;
+            email.Text = order.email;
+            sdt.Text = order.sdt;
+            if (order.tinhtrang == "Đang xử lí") cancle_booking.IsVisible = true;
 
             //order.kieudat la true la theo gio, false la theo ngay
-            follow_time.IsVisible = order.kieudat;
-            follow_day.IsVisible = !order.kieudat;
-            total_time.IsVisible = order.kieudat;
-            total_day.IsVisible = !order.kieudat;
+            if(order.ptdatphong=="Theo giờ")
+            {
+                follow_time.IsVisible = true;
+                follow_day.IsVisible = false;
+                total_time.IsVisible = true;
+                total_day.IsVisible = false;
+                giagio.Text = String.Format("{0:0,0}", order.gia);
+            }
+            else
+            {
+                follow_time.IsVisible = false;
+                follow_day.IsVisible = true;
+                total_time.IsVisible = false;
+                total_day.IsVisible = true;
+                giangay.Text = String.Format("{0:0,0}", order.gia);
+            }
+            if (order.phuongthuc == "online")
+            {
+                ttthanhtoan.IsVisible = true;
+                nganhang.Text = order.nganhang;
+                tennganhang.Text = order.tennganhang;
+                stk.Text = order.sotaikhoan;
+            }
 
             //dientich.Text = "50 m2";
             //desc.Text ="\t"+ thishotel.mota;
             //mota.Text = "\t" + "Phòng xịn nhất";
             //contact.Text = thishotel.lienhe;
-            total.Text = String.Format("{0:0,0}", order.total);
-            request_of_customer.Text = "\t"+ order.customer_request;
+            total.Text = String.Format("{0:0,0}", order.trigia);
+            request_of_customer.Text = "\t"+ order.khac;
 
             //Hiện thông tin các tiện ích mà khách sạn đang có
             //if (thishotel.tienichs.Count > 0)
@@ -110,7 +131,7 @@ namespace BookingHotel.Views
             //}
         }
 
-        public Page_Order_Detail(Order order)
+        public  Page_Order_Detail(HoaDon order)
         {
             InitializeComponent();
             hienthi(order);
@@ -133,12 +154,11 @@ namespace BookingHotel.Views
 
         private void Room_btn_Clicked(object sender, EventArgs e)
         {
-
+            Shell.Current.Navigation.PushAsync(new Page_Room_Info(thishotel,thisroom));
         }
+        //private void book_btn_Clicked(object sender, EventArgs e)
+        //{
 
-        private void book_btn_Clicked(object sender, EventArgs e)
-        {
-
-        }
+        //}
     }
 }
